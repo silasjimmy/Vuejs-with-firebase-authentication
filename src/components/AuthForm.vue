b5fcea682ce7883581079568487ec685ec212e18
Silas Jimmy <jimmysilas17@gmail.com>
2022-03-24 13:40:16 +0300

    add: create authentication form component

---
+++ src/components/AuthForm.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-card>
+    <v-card-title>Title</v-card-title>
+    <v-card-subtitle>Subtitle</v-card-subtitle>
+    <v-card-text>Content</v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "AuthForm",
+};
+</script>
